4cf73e85938bc83588821f9ae41a133f3216da3f
Kenko Nakamura <kenkou.n@gmail.com>
2018-12-31 14:52:14 +0900

    Updated

--- include/PictureBus.h
+++ include/PictureBus.h
@@ -1,29 +1,29 @@
 #ifndef PICTUREBUS_H
 #define PICTUREBUS_H
+#include <vector>
 #include "Cartridge.h"
 #include "Mapper.h"
-#include <vector>
 
 namespace sn {
 class PictureBus {
-public:
+ public:
   PictureBus();
-  Byte read(Address addr);
+  Byte read(Address addr) const;
   void write(Address addr, Byte value);
 
   bool setMapper(Mapper *mapper);
-  Byte readPalette(Byte paletteAddr);
+  Byte readPalette(Byte paletteAddr) const;
 
   void updateMirroring();
 
-private:
+ private:
   std::vector<Byte> m_RAM;
   std::size_t NameTable0, NameTable1, NameTable2,
-      NameTable3; // indices where they start in RAM vector
+      NameTable3;  // indices where they start in RAM vector
 
   std::vector<Byte> m_palette;
 
   Mapper *m_mapper;
 };
-} // namespace sn
-#endif // PICTUREBUS_H
+}  // namespace sn
+#endif  // PICTUREBUS_H
