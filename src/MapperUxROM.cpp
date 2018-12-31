06f289a1467218c14a2fcae8543cbe89ef4c192b
Kenko Nakamura <kenkou.n@gmail.com>
2018-12-31 14:26:44 +0900

    yeah

--- src/MapperUxROM.cpp
+++ src/MapperUxROM.cpp
@@ -4,38 +4,40 @@
 namespace sn {
 MapperUxROM::MapperUxROM(Cartridge &cart)
     : Mapper(cart, Mapper::UxROM), m_selectPRG(0) {
-  if (cart.getVROM().size() == 0) {
-    m_usesCharacterRAM = true;
+  this->m_usesCharacterRAM = cart.getVROM().size() == 0;
+  if (this->m_usesCharacterRAM) {
     m_characterRAM.resize(0x2000);
     LOG(Info) << "Uses character RAM" << std::endl;
-  } else
-    m_usesCharacterRAM = false;
+  }
 
   m_lastBankPtr = cart.getROM().size() - 0x4000;  // last - 16KB
 }
 
 Byte MapperUxROM::readPRG(Address addr) const {
-  if (addr < 0xc000)
+  if (addr < 0xc000) {
     return m_cartridge
         .getROM()[((addr - 0x8000) & 0x3fff) | (m_selectPRG << 14)];
-  else
+  } else {
     return this->m_cartridge.getROM()[m_lastBankPtr + (addr & 0x3fff)];
+  }
 }
 
 void MapperUxROM::writePRG(Address addr, Byte value) { m_selectPRG = value; }
 
 Byte MapperUxROM::readCHR(Address addr) const {
-  if (m_usesCharacterRAM)
+  if (m_usesCharacterRAM) {
     return m_characterRAM[addr];
-  else
+  } else {
     return m_cartridge.getVROM()[addr];
+  }
 }
 
 void MapperUxROM::writeCHR(Address addr, Byte value) {
-  if (m_usesCharacterRAM)
+  if (m_usesCharacterRAM) {
     m_characterRAM[addr] = value;
-  else
+  } else {
     LOG(Info) << "Read-only CHR memory write attempt at " << std::hex << addr
               << std::endl;
+  }
 }
 }  // namespace sn
